418f0a7924968563e541fff6bd0eac3660d5d219
Nabieva Liia <114979808+Kirilich@users.noreply.github.com>
2024-11-18 22:29:09 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,57 +1,95 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <sys/types.h>
 
-#define MAX_SENTENCES 300
-#define MAX_LENGTH 1000
-
-int multipleUppercase (const char *sentence) {
-    int capital_count = 0;
-    for (int i = 0; sentence[i] != '\0'; i++) {
-        if (isupper(sentence[i])) {
-            capital_count++;
-            if (capital_count > 1) {
-                return 1;
-            }
-        }
+int countUppercase(const char *str) {
+    int count = 0;
+    while (*str) {
+        if (isupper(*str)) count++;
+        str++;
     }
-    return 0;
+    return count;
 }
 
-void removeWhitespace (char *sentence) {
-    int i = 0;
-    while (sentence[i] == ' ' || sentence[i] == '\t') {
-        i++;
+void removeTrailingWhitespace(char *str) {
+    size_t len = strlen(str);
+    while (len > 0 && (str[len - 1] == ' ' || str[len - 1] == '\t')) {
+        str[len - 1] = '\0';
+        len--;
     }
-    memmove(sentence, sentence + i, strlen(sentence + i) + 1);
+}
+char *removeWhitespace(char *str) {
+    while (*str == ' ' || *str == '\t') str++;
+    return str;
 }
 
 int main() {
-    char input[MAX_LENGTH];
-    char sentences[MAX_SENTENCES][MAX_LENGTH];
-    int sentenceCount = 0;
-    int formattedCount = 0;
-    fgets(input, MAX_LENGTH, stdin);
-    char *start = input;
-    while (*start) {
-        char *end = strpbrk(start, ".;?");
-        if (!end) break;
-        int length = end - start + 1;
-        strncpy(sentences[sentenceCount], start, length);
-        sentences[sentenceCount][length] = '\0';
-        removeWhitespace(sentences[sentenceCount]);
-        if (strstr(sentences[sentenceCount], "Dragon flew away!") != NULL) {
+    char *text = NULL;
+    size_t size = 0;
+    ssize_t bytesRead = getline(&text, &size, stdin);
+    if (bytesRead == -1) {
+        fprintf(stderr, "Ошибка чтения из файла.\n");
+        return 1;
+    }
+    int originalCount = 0;
+    int filteredCount = 0;
+    size_t maxSentences = 10;
+    char **filteredSentences = malloc(maxSentences * sizeof(char *));
+    if (!filteredSentences) {
+        fprintf(stderr, "Ошибка выделения памяти.\n");
+        free(text);
+        return 1;
+    }
+    const char *ending_phrase = "Dragon flew away!";
+    size_t text_len = strlen(text);
+    if (text_len >= strlen(ending_phrase) && strstr(text + text_len - strlen(ending_phrase), ending_phrase)) {
+        text[text_len - strlen(ending_phrase)] = '\0';
+    }
+    char *currentPosition = text;
+    const char *delimiters = ".;?";
+    while (*currentPosition) {
+        size_t len = strcspn(currentPosition, delimiters);
+        char ending_char = currentPosition[len];
+        if (ending_char == '\0') break;
+        char *sentence_start = removeWhitespace(currentPosition);
+        char *sentence = malloc(len + 2);
+        if (!sentence) {
+            fprintf(stderr, "Ошибка выделения памяти для предложения.\n");
+            free(sentence);
             break;
         }
-        sentenceCount++;
-        start = end + 1;
-    }
-    for (int i = 0; i < sentenceCount; i++) {
-        if (!multipleUppercase(sentences[i])) {
-            printf("%s\n", sentences[i]);
-            formattedCount++;
+        strncpy(sentence, sentence_start, len);
+        sentence[len] = '\0'; 
+        removeTrailingWhitespace(sentence);
+        originalCount++;
+        if (countUppercase(sentence) <= 1) {
+            if (filteredCount >= maxSentences) {
+                maxSentences *= 2;
+                char **new_array = realloc(filteredSentences, maxSentences * sizeof(char *));
+                if (!new_array) {
+                    fprintf(stderr, "Ошибка увеличения памяти для массива предложений.\n");
+                    free(sentence);
+                    break;
+                }
+                filteredSentences = new_array;
+            }
+            filteredSentences[filteredCount] = sentence;
+            filteredCount++;
+        } else {
+            printf("%d\n", countUppercase(sentence));
+            printf(sentence);
+            free(sentence);
         }
+        currentPosition += len + 1;
     }
-    printf("Количество предложений до %d и количество предложений после %d\n", sentenceCount, formattedCount);
+    for (int i = 0; i < filteredCount; i++) {
+        printf("%s\n", filteredSentences[i]);
+        free(filteredSentences[i]);
+    }
+    printf("Количество предложений до %d и количество предложений после %d\n", originalCount, filteredCount);
+    free(filteredSentences);
+    free(text);
     return 0;
 }
